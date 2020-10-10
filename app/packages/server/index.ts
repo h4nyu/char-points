435707f5b4af9bc4277edc94a4cc0720ce98bf87
xinyuan-yao <yao.ntno@gmail.com>
2020-10-10 02:55:06 +0000

    add WAF

--- app/packages/server/index.ts
+++ app/packages/server/index.ts
@@ -1,1 +1,14 @@
-export const foo = (x: any) => x
+import fastify from "fastify";
+const server = fastify()
+
+server.get('/ping', async (request, reply) => {
+  return 'pong\n'
+})
+
+server.listen(80, "0.0.0.0", (err, address) => {
+  if(err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
+})
