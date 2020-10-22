261efd9e489cec118bc447ca974d74e4bc3253ae
xinyuan-yao <yao.ntno@gmail.com>
2020-10-22 14:39:33 +0000

    addons

---
+++ app/packages/web/.storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "stories": [
+    "../**/*.stories.@(ts|tsx)"
+  ],
+  "addons": [
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
+  ],
+}
---
+++ app/packages/web/components/FileUpload.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { FileUpload } from './FileUpload';
+
+export default {
+  title: 'FileUpload',
+};
+
+export const Primary: React.SFC<{}> = () => <FileUpload/>;
+
