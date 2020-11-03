de3ff51e8e7a6d460cbf91fa34d2b5dbd14ceb0e
xinyuan-yao <yao.ntno@gmail.com>
2020-11-03 14:04:41 +0000

    fix

--- app/packages/web/pages/MainPage.tsx
+++ app/packages/web/pages/MainPage.tsx
@@ -1,114 +1,42 @@
 import React from "react";
 import { Header } from "../components/Header";
-import { observer, useObserver } from "mobx-react-lite";
-import { createContainer } from "unstated-next";
+import { observer } from "mobx-react-lite";
+import { observable, makeObservable } from "mobx"
+import { createContainer } from "unstated-next"
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import { observable, autorun } from "mobx";
 
-const Store = () => {
-  let count = 0;
+
+type State = {
+  count:number
+}
+
+const Store = (state:State) => {
   const countUp = () => {
-    count = count + 1;
+    state.count = state.count + 1
   };
+  setInterval(countUp, 1000)
   return {
-    count,
-    countUp: autorun(countUp),
-  };
-};
-const store = observable(Store());
-
-const useStoreA = () => {
-  const [count, setCount] = React.useState(0);
-  console.log("useStoreA");
-  const countUp = () => {
-    setCount(count + 1);
-    console.log(`called ${count}`);
-  };
-  React.useEffect(() => {
-    setInterval(() => {
-      countUp();
-    }, 1000);
-  }, []);
-
-  return {
-    count,
-    countUp,
+    state,
+    countUp: countUp,
   };
 };
 
-const useStoreB = () => {
-  const [count, setCount] = React.useState(0);
-  const countUp = () => {
-    setCount(count + 1);
-  };
-  console.log("useStoreB");
-  return {
-    count,
-    countUp,
-  };
-};
-const StoreA = createContainer(useStoreA);
-const StoreB = createContainer(useStoreB);
+const store = Store(observable({count:0}));
 
-const AB = observer(({}) => {
-  const storeA = StoreA.useContainer();
-  const storeB = StoreB.useContainer();
-  console.log("reander AB");
-  return (
-    <div>
-      {storeA.count}
-      <button onClick={() => storeA.countUp()}>countUp</button>
-      {storeB.count}
-      <button onClick={() => storeB.countUp()}>countUp</button>
-      {store.count}
-      <button onClick={() => store.countUp()}>store</button>
-    </div>
-  );
+const A = observer(() => {
+  return <>
+    <div>{ store.state.count } </div>
+    <button onClick={() => store.countUp()}>aaaa</button>
+  </>
 });
 
-const B = () => {
-  const storeB = StoreB.useContainer();
-  console.log("reander B");
-  return (
-    <div>
-      {storeB.count}
-      <button onClick={() => storeB.countUp()}>countUp</button>
-    </div>
-  );
-};
-const A = () => {
-  const storeA = StoreA.useContainer();
-  console.log("reander A");
-  return (
-    <div>
-      {storeA.count}
-      <button onClick={() => storeA.countUp()}>countUp</button>
-    </div>
-  );
-};
 
 export default function MainPage() {
   return (
-    <StoreB.Provider>
-      <StoreA.Provider>
-        <Header />
-        <Card>
-          <CardContent>
-            <AB />
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent>
-            <A />
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent>
-            <B />
-          </CardContent>
-        </Card>
-      </StoreA.Provider>
-    </StoreB.Provider>
+    <>
+      <Header />
+      <A />
+    </>
   );
 }
--- app/packages/web/store/index.ts
+++ app/packages/web/store/index.ts
@@ -1,16 +1,5 @@
 import React, { useState } from "react";
-import { createContainer } from "unstated-next";
 
-export const useCounter = () => {
-  const [count, setCount] = useState(0);
-  const countUp = () => {
-    setCount(count + 1);
-    console.log(`counterUp ${count}`);
-  };
-  return {
-    count,
-    countUp,
-  };
+export const Counter = () => {
 };
 
-export default createContainer(useCounter);
