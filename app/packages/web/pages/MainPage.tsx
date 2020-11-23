75e8c36d8b92eed38cdc88a6e7ca4c0f01087d21
h4nyu <yao.ntno@gmail.com>
2020-11-23 06:19:26 +0000

    upload

--- app/packages/web/pages/MainPage.tsx
+++ app/packages/web/pages/MainPage.tsx
@@ -10,37 +10,41 @@ const Content = observer(() => {
   const { history } = store;
   const { charImages } = store.data.state;
   const { init } = store.editCharImage;
+  const { uploadFiles } = store.charImage;
   return (
-    <div
-      style={{
-        display: "flex",
-        flexWrap: "wrap",
-        overflow: "auto",
-      }}
-    >
-      {charImages
-        .toList()
-        .sortBy((x) => x.points?.length)
-        .reverse()
-        .map((x) => (
-          <div
-            className="card m-1"
-            key={x.id}
-          >
-            <div className="card-image">
-              <SvgCharPlot data={x.data} points={x.points} size={128} />
+    <>
+      <Upload onChange={uploadFiles}/>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          overflow: "auto",
+        }}
+      >
+        {charImages
+          .toList()
+          .sortBy((x) => x.points?.length)
+          .reverse()
+          .map((x) => (
+            <div
+              className="card m-1"
+              key={x.id}
+            >
+              <div className="card-image">
+                <SvgCharPlot data={x.data} points={x.points} size={128} />
+              </div>
+              <footer className="card-footer">
+                <button className="card-footer-item button" onClick={() => init(x.id)}>
+                  <i className="fas fa-edit"></i>
+                </button>
+                <button className="card-footer-item button">
+                  <i className="fas fa-trash"></i>
+                </button>
+              </footer>
             </div>
-            <footer className="card-footer">
-              <button className="card-footer-item button" onClick={() => init(x.id)}>
-                <i className="fas fa-edit"></i>
-              </button>
-              <button className="card-footer-item button">
-                <i className="fas fa-trash"></i>
-              </button>
-            </footer>
-          </div>
-        ))}
-    </div>
+          ))}
+      </div>
+    </>
   );
 });
 
