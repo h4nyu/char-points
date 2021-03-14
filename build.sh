a32fee4290d2e5761df35252c41817372b9ceeba
xinyuan-yao <yao.ntno@gmail.com>
2021-03-14 12:55:03 +0000

    use buildx

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/shell
 
-docker buildx build --platform linux/arm/v7,linux/arm64,linux/amd64 --tag $DOCKER_REGISTRY/sivic/app --push ./app
+# docker buildx build --platform linux/arm/v7,linux/arm64,linux/amd64 --tag $DOCKER_REGISTRY/charpoints/app --push ./app
+docker buildx build --platform linux/arm/v7,linux/arm64,linux/amd64 --tag $DOCKER_REGISTRY/charpoints/app --push ./app
