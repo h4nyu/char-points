2e5eebd6b51d067ac3b0f3b6e641889df6979cb1
xinyuan-yao <yao.ntno@gmail.com>
2021-04-01 21:19:55 +0900

    fix

--- app/packages/server/bin/start.ts
+++ app/packages/server/bin/start.ts
@@ -10,20 +10,10 @@ const store = Store({
 const lock = Lock({ dir: "/tmp" });
 export const app = App({ store, lock });
 
-if (cluster.isMaster && process.env.NODE_ENV !== "development") {
-  console.log(`Master ${process.pid} is running`);
-  for (const _ of os.cpus()) {
-    cluster.fork();
+app.listen(80, "0.0.0.0", (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
   }
-  cluster.on("exit", (worker) => {
-    console.log(`Worker ${worker.process.pid} died`);
-  });
-} else {
-  app.listen(80, "0.0.0.0", (err, address) => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log(`Slave ${process.pid} Server listening at ${address}`);
-  });
-}
+  console.log(`Slave ${process.pid} Server listening at ${address}`);
+});
